1aa88fd137a27d2975e94fecc2e8d9d2eea93018
zihengh1 <35376020+zihengh1@users.noreply.github.com>
2018-03-21 14:25:04 +0800

    Delete hex_to_dec.py

--- hex_to_dec.py
+++
@@ -1,9 +0,0 @@
-data_in = "4d420000000000130000000000000000000000000000000000000000000000a2"
-new_list = []
-for x in data_in:
-    new_list.append(x)
-
-for x in range(0, len(data_in), 4):
-    print(new_list[x:x+4])
-    ans = int(new_list[x] + new_list[x + 1] + new_list[x + 2] + new_list[x + 3], 16)
-    print(ans)
